b5852f557ce1a46730534e2dc00a2bd3df4056f7
Daniel Schroter <daniel@schroter.biz>
2023-09-20 11:50:01 +0200

    GPT completed

--- v2.py
+++ v2.py
@@ -5,16 +5,16 @@ from torch.nn import functional as F
     
 # Hyperparameters
 
-batch_size = 64 # how many independet sequences to train at once
-block_size = 256 # what is the maximum context length for predictions ?
+batch_size = 16 # how many independet sequences to train at once
+block_size = 128 # what is the maximum context length for predictions ?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
-n_embed = 384
-n_head = 6
-n_layer = 6
+n_embed = 32
+n_head = 4
+n_layer = 4
 dropout = 0.2
 # ---------------
 
