b8037b169a18778d802f183b52f72955f68c2d6d
Daniel Schroter <daniel@schroter.biz>
2023-09-20 10:56:53 +0200

    Dropout and generic variables to scale up

--- v2.py
+++ v2.py
@@ -5,16 +5,17 @@ from torch.nn import functional as F
     
 # Hyperparameters
 
-batch_size = 32 # how many independet sequences to train at once
-block_size = 8 # what is the maximum context length for predictions ?
+batch_size = 64 # how many independet sequences to train at once
+block_size = 256 # what is the maximum context length for predictions ?
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
-n_embed = 32
-n_head = 4
-n_layer = 4
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 # ---------------
 
 torch.manual_seed(1337)
@@ -74,7 +75,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embed, head_size, bias=False)
         self.value = nn.Linear(n_embed, head_size, bias=False)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size))) # its not a parameter, but we want to keep it around, so we register it as a buffer. Pytorch Naming convention.
-        
+        self.dropout = nn.Dropout(dropout)
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x)
@@ -84,7 +85,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-1,-2) * C ** (-0.5) # (B,T,C) @ (B,C,T) -> (B,T,T)
         wei = wei.masked_fill(self.tril[:T, :T]==0, float('-inf')) # (B, T, T) # mask out the upper triangular part of the matrix
         wei = F.softmax(wei, dim=-1) # (B,T,T)
-        
+        wei = self.dropout(wei) # Prevent some of the nodes from communicating 
         v = self.value(x)
         
         out = wei @ v 
@@ -97,10 +98,13 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(num_heads*head_size, n_embed) # This is for residual skip connection. Projection is a linear combination of previous output.
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1) # (B,T,C)
         out = self.proj(out) # (B,T,C)
+        out = self.dropout(out)
+
         return out
         
 
@@ -112,7 +116,8 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embed, 4 * n_embed), # 4 times the embedding dimension, because we want to have a lot of parameters. In paper its 512 -> 2048
             nn.ReLU(),
-            nn.Linear(4 * n_embed, n_embed)
+            nn.Linear(4 * n_embed, n_embed),
+            nn.Dropout(dropout)
         )
         
     def forward(self, x):
